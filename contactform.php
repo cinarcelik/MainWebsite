6eb28422fea131e9d5bc190f99cea0fdd0f4bfb2
Çınar Çelik <59706127+cinarcelik@users.noreply.github.com>
2020-04-09 22:03:24 +0300

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,10 +1,17 @@
 <?php
 
-if(isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $subject = $_POST['subject'];
     $emailFrom = $_POST['email'];
     $message = $_POST['message'];
+
+    $emailTo = "celikcinar@yahoo.com";
+    $headers = "From: ".$emailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+    mail($emailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
 }
 
 ?>
