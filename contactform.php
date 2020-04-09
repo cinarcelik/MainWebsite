117d07e2dd5400f722bfafe76a9da8e025cbd8c9
Çınar Çelik <59706127+cinarcelik@users.noreply.github.com>
2020-04-09 21:24:18 +0300

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,3 +1,10 @@
 <?php
-    
+
+if(isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $emailFrom = $_POST['email'];
+    $message = $_POST['message'];
+}
+
 ?>
